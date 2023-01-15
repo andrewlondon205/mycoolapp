bf2ef74fe3bc6ad6f92f6032fc9e919472eb2b7f
Ramai Alejandria <vyasadev931@gmail.com>
2023-01-15 17:09:17 +0100

    endpoint implementations

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/FunRestController.java
@@ -1,4 +1,28 @@
 package com.luv2code.springboot.demo.mycoolapp.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
+@RestController
 public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello world! time on server is " + LocalDateTime.now();
+    }
+
+    //expose a new endpoint for 'workout'
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5K!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day";
+    }
+
+
+
 }
