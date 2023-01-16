d2bcc8ca5e5ccd418c4fc0a3a01224a9a74d8ca2
Ramai Alejandria <vyasadev931@gmail.com>
2023-01-16 10:50:23 +0100

    app properties configuration

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/FunRestController.java
@@ -1,4 +1,5 @@
 package com.luv2code.springboot.demo.mycoolapp.controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -6,6 +7,18 @@ import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
+
+    //inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    //expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach Name: " + coachName + " , Team Name: " + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello() {
