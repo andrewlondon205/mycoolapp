1e00921e5321bffe9b2f2d030beeeec6a7139e5d
Ramai Alejandria <vyasadev931@gmail.com>
2023-01-16 11:16:05 +0100

    dev tools

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/FunRestController.java
@@ -28,12 +28,12 @@ public class FunRestController {
     //expose a new endpoint for 'workout'
     @GetMapping("/workout")
     public String getDailyWorkout() {
-        return "Run a hard 5K!";
+        return "Today i am going to the gym to do pushups!";
     }
 
     @GetMapping("/fortune")
     public String getDailyFortune() {
-        return "Today is your lucky day";
+        return "Today is a very lucky day for you";
     }
 
 
